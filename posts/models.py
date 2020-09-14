ce7599649b959cb713a16e42963cd294ac351296
Theodore Charts <tedcharts@gmail.com>
2020-09-14 19:59:57 -0400

    inital commit

---
+++ posts/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    text_content = models.CharField(max_length=160)
+    image_content = models.ImageField(
+        upload_to='images/',
+        blank=True,
+        null=True
+        )
+    likes = models.ManyToManyField(User, related_name='post_likes')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_date = models.DateTimeField(auto_now_add=True)
+    last_edited_date = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    text_content = models.CharField(max_length=160)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+        )
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_date = models.DateTimeField(auto_now_add=True)
+    last_edited_date = models.DateTimeField(auto_now=True)
